796d15266b33b9c316b85f92cb8161e5ffa98e5a
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-03-23 18:19:53 +0530

    added images to the dice

---
+++ 06_dice_roller/script.js
@@ -0,0 +1,19 @@
+    let btns = document.getElementsByClassName("button");
+    let faces = document.getElementsByClassName("Dice-face");
+    let transition = document.getElementsByClassName("showDice")
+    let n = 6;
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function() {
+            n = Math.floor(Math.random() * 6) + 1;
+            console.log(n);
+
+            // Hide all faces first
+            for (let j = 0; j < 6; j++) {
+                faces[j].style.display = "none";
+            }
+            
+            // Display the selected face
+            faces[n - 1].style.display = "block";
+        });
+    }
